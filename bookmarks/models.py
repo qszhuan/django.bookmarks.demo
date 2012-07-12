3d1e5429686b5518d14da51bd9e023cf2a995d4f
twer <twer@twer.(none)>
2012-07-12 22:36:59 +0800

    add tag model and clean

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -9,3 +9,6 @@ class Bookmark(models.Model):
     link = models.ForeignKey(Link)
     title = models.CharField(max_length=200)
 
+class Tag(models.Model):
+    name = models.CharField(unique=True, max_length=40)
+    bookmarks = models.ManyToManyField(Bookmark)
